98ece9cc54ed5a315d9a6e48c8b7fee105fd0a95
Oleksa <oleksa.antsupov@gmail.com>
2022-12-20 16:50:59 +0200

    add starter kit

---
+++ src/services/MarvelService.js
@@ -0,0 +1,18 @@
+
+class MarvelService {
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResource('https://gateway.marvel.com:443/v1/public/characters?apikey=eab153c3569ea3d9983a6d18ddc1ac3e');
+    }
+}
+
+export default MarvelService;
