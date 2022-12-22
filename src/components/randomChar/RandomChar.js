ba8afa0a66fb65921f25e736bdb7c811320f16f0
Oleksa <oleksa.antsupov@gmail.com>
2022-12-22 12:09:07 +0200

    add loading in state

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import Spinner from '../spinner/Spinner';
 import MarvelService from '../../services/MarvelService';
 
 import './randomChar.scss';
@@ -11,7 +12,8 @@ class RandomChar extends Component {
     }
 
     state = {
-        char: {}
+        char: {},
+        loading: true
     }
     
     marvelService = new MarvelService();
@@ -28,7 +30,12 @@ class RandomChar extends Component {
     }
 
     render() {
-        const {char: {name, description, thumbnail, homepage, wiki}} = this.state;
+        const {char: {name, description, thumbnail, homepage, wiki}, loading} = this.state;
+
+        if (loading) {
+            return <Spinner/>
+        }
+
         return (
             <div className="randomchar">
                 <div className="randomchar__block">
