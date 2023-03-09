1d640d39e8f2835bef0bcac79ba68a06e0a9b3c8
Oleksa <oleksa.antsupov@gmail.com>
2023-03-09 13:30:17 +0200

    update index.js for supprort React 18

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './components/app/App';
 
 import './style/style.scss';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
