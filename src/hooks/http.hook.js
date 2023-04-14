4f9d99157b1f6f6494210448936eceba7010c420
Oleksa <oleksa.antsupov@gmail.com>
2023-04-14 19:58:42 +0300

    some fix

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -12,7 +12,7 @@ export const useHttp = () => {
             const response = await fetch(url, {method, body, headers});
 
             if (!response.ok) {
-                throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+                throw new Error(`Could not fetch ${url}, status: ${response.status}`);
             }
 
             const data = await response.json();
